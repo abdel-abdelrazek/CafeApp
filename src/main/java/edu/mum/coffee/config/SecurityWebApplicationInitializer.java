73c0cf5c2368c872b612f4fa501fe4adedecf19f
abdelrahman91 <38641993+abdelrahman91@users.noreply.github.com>
2018-06-09 13:22:43 -0500

    Revert "Product List"
    This reverts commit 36b9ca59f4f480c56e4c5dbca50ec2dc62ac9bd7.

--- src/main/java/edu/mum/coffee/config/SecurityWebApplicationInitializer.java
+++
@@ -1,8 +0,0 @@
-package edu.mum.coffee.config;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityWebApplicationInitializer 
-						extends AbstractSecurityWebApplicationInitializer {
-
-}
--- src/main/java/edu/mum/coffee/controller/HomeController.java
+++ src/main/java/edu/mum/coffee/controller/HomeController.java
@@ -1,24 +1,13 @@
 package edu.mum.coffee.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import edu.mum.coffee.service.ProductService;
 
 @Controller
 public class HomeController {
 	
-	
-	@Autowired
-	ProductService productService;
-	
 	@GetMapping({"/", "/index", "/home"})
-	public String homePage(Model model) {
-		
-		model.addAttribute("prods", productService.getAllProduct());
-		
+	public String homePage() {
 		return "index";
 	}
 
