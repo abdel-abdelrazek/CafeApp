88b2d1a8642174542202cff18dc493ccab1c22aa
abdelrahman91 <38641993+abdelrahman91@users.noreply.github.com>
2018-06-09 13:32:47 -0500

    Revert "Revert "Product List""
    This reverts commit 73c0cf5c2368c872b612f4fa501fe4adedecf19f.

---
+++ src/main/java/edu/mum/coffee/config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,8 @@
+package edu.mum.coffee.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer 
+						extends AbstractSecurityWebApplicationInitializer {
+
+}
