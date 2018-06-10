392790dbd435e5790b9fb8e768f709c892dce3ee
abdelrahman91 <38641993+abdelrahman91@users.noreply.github.com>
2018-06-10 18:30:42 -0500

    rest usage

--- src/main/java/edu/mum/coffee/controller/GeneralController.java
+++ src/main/java/edu/mum/coffee/controller/GeneralController.java
@@ -1,10 +1,15 @@
 package edu.mum.coffee.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
 
+import edu.mum.coffee.domain.Product;
 import edu.mum.coffee.general.NoSuchResourceException;
 
 
@@ -31,6 +36,21 @@ public class GeneralController {
 	public String exceptionHandeler() {
 		
 		return "exceptionHandeling";
+		
+	}
+	
+	@GetMapping("/rest")
+	public String testRest(Model moedel) {
+		
+		RestTemplate restTemplate = new RestTemplate();
+		String products
+		  = "http://localhost:8090/product/all";
+		ResponseEntity<Product> response
+		  = restTemplate.getForEntity(products , Product.class);
+		
+		moedel.addAttribute("products", response);
+		
+		return "restTest";
 		
 	}
 }
